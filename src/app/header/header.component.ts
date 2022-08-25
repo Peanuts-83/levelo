38ed22af1261e33c98e7c7b6c0c1bd92a01d4753
Peanuts-83 <tranque@free.fr>
2022-08-25 16:11:35 +0200

    responsive + nav show/hide

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { fromEvent, Observable, Subscription } from 'rxjs'
+import { Component, OnInit, ViewChild, ElementRef, HostListener, OnDestroy, EventEmitter } from '@angular/core'
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  @ViewChild('menu') menu: ElementRef
+  innerWidth: number = 768
+  private resizeObservable$: Observable<Event>
+  private resizeSubscription$: Subscription
+
+  // Resize listener
+  onResize(event) {
+    this.innerWidth = event.target.innerWidth
+    if (this.innerWidth >= 768) {
+      // console.log('SHOW')
+      this.menu.nativeElement.style.display = 'flex'
+      this.menu.nativeElement.style.opacity = 1
+    } else {
+      // console.log('HIDE')
+      this.menu.nativeElement.style.display = 'none'
+    }
+    console.log(this.innerWidth)
+  }
+
+  // Show/Hide menu
+  displayMenu(show: boolean) {
+    // console.log('display()')
+    if (this.innerWidth >= 768) return
+    if (show) {
+      this.menu.nativeElement.style.display = 'flex'
+      setTimeout(() => {
+        this.menu.nativeElement.style.opacity = 1
+        this.menu.nativeElement.style.transform = 'translateY(0)'
+      }, 50)
+    } else {
+      this.menu.nativeElement.style.opacity = 0
+      this.menu.nativeElement.style.transform = 'translateY(10px)'
+      setTimeout(() => {
+        this.menu.nativeElement.style.display = 'none'
+      }, 300)
+    }
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    this.resizeObservable$ = fromEvent(window, 'resize')
+    this.resizeSubscription$ = this.resizeObservable$.subscribe(e => {
+      this.onResize(e)
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.resizeSubscription$.unsubscribe()
   }
 
 }
