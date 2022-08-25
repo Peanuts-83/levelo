22313202513e78ba8b23769a4c42306ad06c5b34
Peanuts-83 <tranque@free.fr>
2022-08-25 16:31:14 +0200

    router done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 // import { LegendComponent } from './map/legend/legend.component';
 import { MapComponent } from './map/map.component'
+import { CommentComponent } from './comment/comment.component'
+import { MagazineComponent } from './magazine/magazine.component'
+import { SecuriteComponent } from './securite/securite.component'
+import { TicketsComponent } from './tickets/tickets.component'
 
 const routes: Routes = [
-  {path: '', component: MapComponent}
+  {path: '', component: MapComponent},
+  {path: 'comment_ça_marche', component: CommentComponent},
+  {path: 'tickets', component: TicketsComponent},
+  {path: 'magazine', component: MagazineComponent},
+  {path: 'securite', component: SecuriteComponent},
+  {path: '**', component: MapComponent},
+
 ];
 
 @NgModule({
